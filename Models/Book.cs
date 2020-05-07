93bba678e60c0d63bdd5fec41fbc669168af944e
zhouyufei1 <zhou.2153@osu.edu>
2020-05-07 15:59:19 -0400

    a

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,6 +6,12 @@ namespace Fisher.Bookstore.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public List<Author> Authors { get; set; }
+        public double Price { get; set; }
+        public List<BookAuthor> BookAuthor { get; set; }
+
+        public void ApplyMemberDiscount()
+        {
+            Price = Price * .90;
+        }
     }
 }
